670fab43f6f5f7f56ce1d10499f2d4d8657568dd
gmgpx <gugapires92@gmail.com>
2025-06-03 20:08:57 -0300

    feat: add comments in local storage

--- prova/av/app/(tabs)/main.tsx
+++ prova/av/app/(tabs)/main.tsx
@@ -1,6 +1,7 @@
-import React, { useState } from 'react';
-import { View, Text, Button, ActivityIndicator, StyleSheet } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, Text, Button, ActivityIndicator, StyleSheet, TouchableOpacity } from 'react-native';
 import axios from 'axios';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 interface Comment {
   postId: number;
@@ -13,12 +14,17 @@ interface Comment {
 export default function PaginaC() {
   const [comment, setComment] = useState<Comment | null>(null);
   const [loading, setLoading] = useState(false);
+  const [savedCount, setSavedComment] = useState(0);
+
+  useEffect(() => {
+    countSavedComments();
+  }, []);
 
   const fetchComment = async () => {
     setLoading(true);
     try {
       const response = await axios.get<Comment[]>('https://jsonplaceholder.typicode.com/comments');
-      const data = response.data[0]; 
+      const data = response.data[Math.floor(Math.random() * response.data.length)];
       setComment(data);
     } catch (error) {
       console.error('Erro ao buscar comentário:', error);
@@ -27,19 +33,46 @@ export default function PaginaC() {
     }
   };
 
+  const saveComment = async () => {
+    if (comment) {
+      try {
+        const stored = await AsyncStorage.getItem("savedComments");
+        const comments = stored ? JSON.parse(stored) : [];
+        comments.push(comment);
+        await AsyncStorage.setItem("savedComments", JSON.stringify(comments));
+        countSavedComments();
+      } catch (error) {
+        console.error('Erro ao salvar comentário:', error);
+      }
+    }
+  };
+
+  const countSavedComments = async () => {
+    try {
+      const stored = await AsyncStorage.getItem("savedComments");
+      const comments = stored ? JSON.parse(stored) : [];
+      setSavedComment(comments.length);
+    } catch (error) {
+      console.error('Erro ao contar comentários salvos:', error);
+    }
+  };
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Página C</Text>
+      <Text style={styles.title}>Total salvos: {savedCount}</Text>
       <Button title="Buscar comentário" onPress={fetchComment} />
       {loading && <ActivityIndicator size="large" style={styles.loader} />}
       {comment && !loading && (
-        <View style={styles.Box}>
+        <TouchableOpacity style={styles.Box} onPress={saveComment}>
           <Text style={styles.Text}>ID: {comment.id}</Text>
           <Text style={styles.Text}>Nome: {comment.name}</Text>
           <Text style={styles.Text}>Email: {comment.email}</Text>
           <Text style={styles.Text}>Comentário: {comment.body}</Text>
-        </View>      )}
-    </View>
+          <Text style={styles.Text}>- Clique para salvar! -</Text>
+        </TouchableOpacity>
+      )}
+    </View >
   );
 }
 
