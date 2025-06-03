214b41fed69bd3f413106e75525eaa1a13e700d7
gmgpx <gugapires92@gmail.com>
2025-06-03 19:46:57 -0300

    feat: request comments

--- prova/av/app/(tabs)/main.tsx
+++ prova/av/app/(tabs)/main.tsx
@@ -1,27 +1,27 @@
 import React, { useState } from 'react';
 import { View, Text, Button, ActivityIndicator, StyleSheet } from 'react-native';
+import axios from 'axios';
 
-interface Fact {
-  id: string;
-  text: string;
-  source: string;
-  source_url: string;
-  language: string;
-  permalink: string;
+interface Comment {
+  postId: number;
+  id: number;
+  name: string;
+  email: string;
+  body: string;
 }
 
 export default function PaginaC() {
-  const [fact, setFact] = useState<Fact | null>(null);
+  const [comment, setComment] = useState<Comment | null>(null);
   const [loading, setLoading] = useState(false);
 
-  const fetchFact = async () => {
+  const fetchComment = async () => {
     setLoading(true);
     try {
-      const response = await fetch('https://uselessfacts.jsph.pl/api/v2/facts/random?language=en');
-      const data: Fact = await response.json();
-      setFact(data);
+      const response = await axios.get<Comment[]>('https://jsonplaceholder.typicode.com/comments');
+      const data = response.data[0]; 
+      setComment(data);
     } catch (error) {
-      console.error('Erro ao buscar fato:', error);
+      console.error('Erro ao buscar comentário:', error);
     } finally {
       setLoading(false);
     }
@@ -30,18 +30,15 @@ export default function PaginaC() {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Página C</Text>
-      <Button title="Buscar Fato" onPress={fetchFact} />
+      <Button title="Buscar comentário" onPress={fetchComment} />
       {loading && <ActivityIndicator size="large" style={styles.loader} />}
-      {fact && !loading && (
-        <View style={styles.factBox}>
-          <Text style={styles.factText}>ID: {fact.id}</Text>
-          <Text style={styles.factText}>Fact: {fact.text}</Text>
-          <Text style={styles.factText}>Source : {fact.source}</Text>
-          <Text style={styles.factText}>URL: {fact.source_url}</Text>
-          <Text style={styles.factText}>Language: {fact.language}</Text>
-          <Text style={styles.factText}>Link: {fact.permalink}</Text>
-        </View>
-      )}
+      {comment && !loading && (
+        <View style={styles.Box}>
+          <Text style={styles.Text}>ID: {comment.id}</Text>
+          <Text style={styles.Text}>Nome: {comment.name}</Text>
+          <Text style={styles.Text}>Email: {comment.email}</Text>
+          <Text style={styles.Text}>Comentário: {comment.body}</Text>
+        </View>      )}
     </View>
   );
 }
@@ -50,6 +47,6 @@ const styles = StyleSheet.create({
   container: { flex: 1, padding: 24, justifyContent: 'center' },
   title: { fontSize: 32, textAlign: 'center', marginBottom: 40, color: 'royalblue' },
   loader: { marginTop: 24 },
-  factBox: { marginTop: 24, backgroundColor: 'snow', padding: 16, borderRadius: 16 },
-  factText: { fontSize: 16, marginVertical: 8, textAlign: 'center' },
+  Box: { marginTop: 24, backgroundColor: 'snow', padding: 16, borderRadius: 16 },
+  Text: { fontSize: 16, marginVertical: 8, textAlign: 'center' },
 });
